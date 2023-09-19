faec0d471e065fa03e36ecc7b0b70bcadd5c926f
HipoTofu <141717556+HipoTofu@users.noreply.github.com>
2023-09-19 16:03:18 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -7,8 +7,8 @@ export default function Home() {
     <Layout home>
       
       <Head>
-        <title>Manifesto For Agile Software Development</title>
       </Head>
+            <title>Manifesto For Agile Software Development</title>
         <p>
           We are uncovering better ways of developing
           software by doing it and helping others do it.
